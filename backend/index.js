d42d6c8636d84310bbd2b938203e6e1d102d9978
jugalNoob <sjugal126@gmail.com>
2024-10-21 20:02:44 -0700

    'cluster'

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const cookieParser = require('cookie-parser');
+const startServer = require('./Cluster/clust')
 const app=express()
 const router = require('./routes/router');
+
 const cors=require('cors')
 require('./db/conn')
 require('dotenv').config();
@@ -16,18 +18,53 @@ const corsOption = {
 
 
 
-
 ///all app.use 
-const Port=process.env.PORT // .env port number is 041
+const Port=process.env.PORT || 9000  // .env port number is 041
+startServer(app, Port);  /// function loadbalance use for cluster management 
+
+
 app.use(express.json())
 app.use(cors(corsOption));
 app.use(router)
 app.use(cookieParser());
 
 
+module.exports = app;
 
-app.listen(Port , ()=>{
-    console.log(`Server is running on port ${Port}`)
-})
+
+
+// app.listen(Port , ()=>{
+//     console.log(`Server is running on port ${Port}`)
+// })
+
+
+
+// const express = require('express');
+// const router = require('./routes/router');
+// const startServer = require('./Cluster/clust');
+// const morgan = require('morgan');
+// const start=process.hrtime()
+
+
+
+
+// const app = express();
+// const port = 9000;
+
+// app.use(router);
+
+
+// app.use(morgan('combined'))  /// morgan for respones:: get information 
+
+// startServer(app, port);  /// function loadbalance use for cluster management 
+// const end=process.hrtime(start)
+
+// const latencyInMs=end[0]*1000+end[1]/1e6;
+
+// console.log(`Latency: ${latencyInMs} ms`)  //check if latency
+
+
+// // Start the server using cluster management
+// module.exports = app;
 
 
